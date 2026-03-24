3398eb51f2804c6e161243d39117c89e7c354959
Swapnil Patil <swapnilpatil9035@gmail.com>
2026-03-24 21:56:20 +0530

    Time: 3 ms (43.44%), Space: 19.2 MB (94.6%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        count = Counter(nums1)
+        res = []
+
+        for num in nums2:
+            if count[num] > 0:
+                res.append(num)
+                count[num] -= 1
+
+        return res
