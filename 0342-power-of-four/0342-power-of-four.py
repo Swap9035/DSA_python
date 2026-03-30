1f7467a3057f31ce0e89fe15c9048567a4955e95
Swapnil Patil <swapnilpatil9035@gmail.com>
2026-03-30 18:35:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (74.83%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -3,7 +3,7 @@ class Solution:
         if n <= 0:
             return False
         while n % 4 == 0:
-            n //= 4
-        return n ==1
+            n //= 4     # If n is divisible by 4 → keep dividing   You're basically checking:  “Can I keep dividing by 4 until I reach 1?”
+        return n == 1   # You reach 1 → valid power of 4 
         
         
