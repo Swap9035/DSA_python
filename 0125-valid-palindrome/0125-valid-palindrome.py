129ecf0f65b8c8010e8089ce3415cd163ff5e5d2
Swapnil Patil <swapnilpatil9035@gmail.com>
2026-08-29 19:52:21 +0530

    Time: 11 ms (31.52%), Space: 19.6 MB (77.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0 
+        left = 0
         right = len(s) - 1
         while left < right:
             while left < right and not s[left].isalnum():
@@ -9,14 +9,11 @@ class Solution:
                 right -=1
             if s[left].lower() != s[right].lower():
                 return False
+                left +=1
             left +=1
             right -=1
         return True
-       
-       
-
+        
             
-
-
 
         
