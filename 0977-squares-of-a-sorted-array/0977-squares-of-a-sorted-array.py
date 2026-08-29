9c16ea990ca5966c18ca5ddedb914ef5ceb2a5a3
Swapnil Patil <swapnilpatil9035@gmail.com>
2026-08-29 20:06:30 +0530

    Time: 11 ms (54.01%), Space: 20.8 MB (87.31%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -3,21 +3,15 @@ class Solution:
         left = 0
         right = len(nums) - 1
         index = len(nums) - 1
-        res = [0] * len(nums)
-        while left <=right:
-            if (abs(nums[left]) > abs(nums[right])):
-                res[index] = nums[left] ** 2
+        res = [0]* len(nums)
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                res[index] = nums[left]**2
                 left +=1
             else:
-                res[index] = nums[right] ** 2
+                res[index] = nums[right]**2
                 right -=1
             index -=1
-
         return res
-
-
-
-
-        
 
         
